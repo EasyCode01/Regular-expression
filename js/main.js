d24674b5dce8db8e75c5bbafe9f1644bbd80ef60
Easycode01 <ezekielayodele1@gmail.com>
2023-03-06 21:01:53 +0100

    telephone field completed

--- js/main.js
+++ js/main.js
@@ -1,4 +1,31 @@
-let reg = /[a-z]/gi;
+// creating patterns 
+
+// telephone pattern
+
+const inputs = document.querySelectorAll('input');
+
+const patterns = {
+    telephone: /^\d{11}$/,
+}
+function validate(field, regex){
+    //console.log(regex.test(field.value));
+
+    if(regex.test(field.value)){
+        field.className = 'valid';
+    }
+
+    else {
+        field.className = 'invalid';
+    }
+}
+
+inputs.forEach(input => {
+    input.addEventListener('input', (e) =>{
+        // console.log(e.target.attributes.name.value)
+
+        validate(e.target, patterns[e.target.attributes.name.value])
+    })
+})
 
 
 
