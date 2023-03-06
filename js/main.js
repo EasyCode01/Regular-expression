64f0d3c404bf24ddc9db6ef0d3763d99281e1fb0
Easycode01 <ezekielayodele1@gmail.com>
2023-03-06 19:57:46 +0100

    How to create a regular expression

---
+++ js/main.js
@@ -0,0 +1,72 @@
+let reg = /[a-z]/gi;
+
+
+
+
+
+
+// ------------ Notes ----------------
+
+// 1. matching character set
+// [ng] ninja , ginja
+//[abc123] 000
+//[^pe] match all characters except 1 or 2
+
+//2. Ranges
+//[a-z] match all alphabets
+
+// lowercase range and upper case range
+
+//[a-zA-z]
+
+// Numbers
+//[0-9]
+
+//Phone numbers
+//[0-9][0-9]...[0-9]
+
+//3. Repeating Characters
+
+//[0-9]+ any number of character long at least one
+
+// strict length
+// [0-9]{11} match this eleven number of times concurrently 
+// [a-z]{11} match eleven letter words
+
+// [a-z]{5,8} //match between 5 to 8 letter words 
+
+
+// Meta characters
+// \d match any digit (same as [0-9])
+// \w match any word characters(a-z, A-Z, 0-9 ans _'s)
+// \s match a white space character (spaces and tabs)
+// \t match a tab character only
+
+// d-- 'd'
+// \d any digit character test regex101
+
+// e.g \d\s\w
+
+// special characters
+
+//+ The one or more quantifier
+// \
+// [] the character set
+//[^] the negate symbol in a character set
+
+// ? the Zero or one quantifier makes a precedding character optional
+// e.g /hello?/ hello or hell
+
+// . any character whatsoever  (except the new line character)
+
+// * The 0-or-more quantifier (a bit like plus)
+
+// Lesson 8
+// Starting and Ending pattern
+// e.g 5 exact character caret and $
+// caret ^[a-5]{5}
+
+// Lesson 9
+
+// Alternate character | 
+// p | t , (p|t)yre
